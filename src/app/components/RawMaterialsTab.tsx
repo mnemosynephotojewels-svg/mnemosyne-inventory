403001972546f81242e9bf872ba62bfb789f9029
mnemosynephotojewels-svg <mnemosyne.photojewels@gmail.com>
2026-04-28 17:05:38 +0800

    Update RawMaterialsTab.tsx

--- src/app/components/RawMaterialsTab.tsx
+++ src/app/components/RawMaterialsTab.tsx
@@ -603,12 +603,9 @@ export function RawMaterialsTab({
                     onChange={(e) => setUnit(e.target.value)}
                   >
                     <option value="pcs">Pieces (pcs)</option>
-                    <option value="kg">Kilograms (kg)</option>
-                    <option value="g">Grams (g)</option>
-                    <option value="m">Meters (m)</option>
-                    <option value="cm">Centimeters (cm)</option>
-                    <option value="l">Liters (l)</option>
-                    <option value="ml">Milliliters (ml)</option>
+                    <option value="kg">Bundle</option>
+                    <option value="g">Roll</option>
+                    <option value="m">Pack</option>
                   </select>
                 </div>
 
@@ -774,12 +771,9 @@ export function RawMaterialsTab({
                   onChange={(e) => setEditMaterialUnit(e.target.value)}
                 >
                   <option value="pcs">Pieces (pcs)</option>
-                  <option value="kg">Kilograms (kg)</option>
-                  <option value="g">Grams (g)</option>
-                  <option value="m">Meters (m)</option>
-                  <option value="cm">Centimeters (cm)</option>
-                  <option value="l">Liters (l)</option>
-                  <option value="ml">Milliliters (ml)</option>
+                  <option value="kg">Bundle</option>
+                  <option value="g">Roll</option>
+                  <option value="m">Pack</option>
                 </select>
               </div>
 
